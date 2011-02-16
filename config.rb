1049ee6863deb32256aff046de6877c6d9fd23c3
Vidal <vidal.vanbergen@gmail.com>
2011-02-16 13:33:51 +0100

    First commit.

---
+++ config.rb
@@ -0,0 +1,25 @@
+# Require any additional compass plugins here.
+# require "css3-ribbons"
+# require "susy"
+# require "lemonade"
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+
+css_dir = "Google Mail Snow Leopard.safariextension"
+sass_dir = ""
+# fonts_dir = "media/fonts"
+images_dir = "Google Mail Snow Leopard.safariextension/images"
+# javascripts_dir = "media/js"
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+preferred_syntax = :sass
+
+# Indicates whether line comments should be added to compiled css that says where the selectors were defined.
+# Defaults to false in production mode, true in development mode.
+line_comments = false
+
+# The output style for the compiled css. One of: :nested, :expanded, :compact, or :compressed.
+output_style = :nested
+# output_style = :compressed
